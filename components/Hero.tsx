1ed0dad3afcb321c045df2791ead81e4b5576dbd
cg9822@gmail.com <cg9822@gmail.com>
2023-02-18 14:24:05 +0100

    animation added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,37 @@
 import styles from "@/styles/Hero.module.css";
 import { useState } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const [productBackground, setProductBackground] = useState(
     `${styles.backgroundProductOne}`
   );
   const [productName, setProductName] = useState("arise");
+  const [nextProduct, setNextProduct] = useState(
+    `${styles.backgroundProductTwo}`
+  );
+  const [productInfo, setProductInfo] = useState("Crusious minimalistic");
 
   const handleClick = () => {
     if (productBackground === `${styles.backgroundProductOne}`) {
       setProductBackground(`${styles.backgroundProductTwo}`);
       setProductName("black");
+      setNextProduct(`${styles.backgroundProductOne}`);
+      setProductInfo("Outdoor furniture");
     } else {
       setProductBackground(`${styles.backgroundProductOne}`);
       setProductName("arise");
+      setNextProduct(`${styles.backgroundProductTwo}`);
+      setProductInfo("Crusious minimalistic");
     }
   };
 
   return (
     <>
       <div className={`${productBackground} h-full grid grid-cols-3 pb-20`}>
-        <div className="grid grid-rows-2">
+        <div className="flex flex-col-reverse">
+          {/* <div className="grid grid-rows-2">
           <div className="w-full flex justify-center items-end">
             <div className="w-64 h-32 grid grid-cols-8">
               <div
@@ -32,11 +42,21 @@ const Hero = () => {
               </div>
               <div className="col-span-6 bg-[url('/assets/coca-cola-bg.jpg')] bg-cover bg-no-repeat bg-center"></div>
             </div>
+          </div> */}
+          <div className="w-full h-6 flex items-center justify-center gap-2">
+            <div className="rounded-full bg-black w-2 h-2"></div>
+            <div className="rounded-full bg-gray-400 w-2 h-2"></div>
+            <div className="rounded-full bg-gray-400 w-2 h-2"></div>
           </div>
-          <div className="grid w-full h-44 self-end">
-            <h1 className="text-9xl font-extrabold productName text-center">
+          <div className="grid w-full h-fit self-end" key={productName}>
+            <motion.h1
+              className="text-9xl font-extrabold productName text-center"
+              initial={{ opacity: 0, translateX: -100 }}
+              animate={{ opacity: 1, translateX: 0 }}
+              transition={{ type: "spring", duration: 0.6 }}
+            >
               {productName}{" "}
-            </h1>
+            </motion.h1>
           </div>
         </div>
         <div className="grid place-items-end grid-cols-2">
@@ -51,9 +71,22 @@ const Hero = () => {
         </div>
         <div className="relative">
           <div className="absolute right-0 bottom-0 w-96 h-44 bg-[#252525] grid grid-cols-8">
-            <div className="col-span-6 bg-[url('/assets/cucharas-bg.jpg')] bg-center bg-no-repeat bg-cover "></div>
             <div
-              className="col-span-2 grid place-items-center"
+              className={`${nextProduct} col-span-6 flex flex-col justify-center p-8 gap-3`}
+              key={productInfo}
+            >
+              <h5 className="text-gray-50 text-base font-semibold">Next up</h5>
+              <motion.h3
+                initial={{ opacity: 0, translateX: -100 }}
+                animate={{ opacity: 1, translateX: 0 }}
+                transition={{ type: "spring", duration: 0.6 }}
+                className="text-gray-50 text-2xl"
+              >
+                {productInfo}{" "}
+              </motion.h3>
+            </div>
+            <div
+              className="col-span-2 grid place-items-center cursor-pointer"
               onClick={handleClick}
             >
               <FaAngleRight size={20} color="white" />
